f3d95291dd1b20576bbfc21f3b535ce79732ac81
Maxime <maxime.larchez@orange.fr>
2022-11-16 12:47:07 +0100

    add page Product

---
+++ front/js/index.js
@@ -0,0 +1,73 @@
+// Selection du conteneur d'article
+const articleContainer = document.getElementById("items");
+
+// =============================================================================
+// Recuperation des produits de l'API
+
+const getProducts = async function () {
+    const fetchApi = await fetch("http://localhost:3000/api/products")
+    const products = await fetchApi.json()
+    return products   
+}
+getProducts()
+
+// console.log(getProducts)
+// =============================================================================
+// Produit de l'API
+
+const products = await getProducts()
+
+// creation des elements a integrer
+
+function creationArticle(product) {
+
+    const createA = document.createElement('a')
+    const createArticle = document.createElement('article')
+    const createImg = document.createElement('img')
+    const createH3 = document.createElement('h3')
+    const createP = document.createElement('p')
+
+// =============================================================================
+// integration des elements dans le html
+
+    document.getElementById('items').appendChild(createA)
+    createA.insertAdjacentElement('afterbegin',createArticle)
+    createArticle.insertAdjacentElement('afterbegin', createImg)
+    createImg.insertAdjacentElement('afterend',createH3)
+    createH3.insertAdjacentElement('afterend', createP)
+
+// console.log(createA)
+
+// =============================================================================
+// integration des infos dans le html sur les element creer
+
+    createH3.textContent = product.name;
+    createP.textContent = product.description;
+    createImg.src = product.imageUrl;
+    createImg.alt = product.altTxt;
+    createA.href =`./product.html?id=${product._id}`
+
+// console.log(createA)
+}
+
+// =============================================================================
+// creation boucle = pour chaque produit , crée un article
+
+products.forEach(product => {
+    creationArticle(product) 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ front/js/product.js
@@ -0,0 +1,89 @@
+// ============================================================================
+// fonction permettant de trouver l'ID dans l'API sous format .Json
+
+// function getProduct(){
+//     fetch(`http://localhost:3000/api/products/${id}`)
+//     .then((result) => result.json())
+//     .then((data) => {
+
+//         console.log(data)
+//     });
+// }
+// getProduct();
+
+// console.log(getProduct)
+
+// ============================================================================
+// ============================================================================
+// RECUPERATION DE L'ID DU PRODUIT
+
+const urlId = window.location.search ;
+
+const urlIdSearch = new URLSearchParams (urlId);
+
+const id = urlIdSearch.get("id");
+
+// console.log(id)
+
+// APPEL DE L'API POUR RECUPERE L'ENSEMBLE DES INFOS DU PRODUIT
+const getProduct = async function () {
+    const fetchApi = await fetch(`http://localhost:3000/api/products/${id}`)
+    const product = await fetchApi.json()
+    return product;
+}
+// recuperation du produit 
+const product = await getProduct()
+
+console.log(product)
+
+
+// =============================================================================
+// =============================================================================
+// INTEGRATION DES INFORMATIONS DU PRODUIT
+
+
+function displayProductInfos (){
+    
+    //*H1*
+    const titleH1 = document.getElementById('title')
+    titleH1.innerHTML = product.name
+    // console.log(titleH1)
+
+    //*P*
+    const descriptionP = document.getElementById('description')
+    descriptionP.innerHTML = product.description
+    // console.log(descriptionP)
+
+    //*IMG*
+    const imageContainer = document.querySelector('.item__img')
+    const image = document.createElement('img')
+    image.src = product.imageUrl
+    image.alt = product.altTxt
+    imageContainer.appendChild(image)
+
+    // console.log(image)
+
+    // PRICE
+    const priceContainer = document.getElementById('price')
+    const priceProduct = document.createElement('price')
+    priceProduct.innerHTML = product.price
+    priceContainer.appendChild(priceProduct)
+
+    product.colors.forEach(color =>{
+        const colorOption = document.createElement('option')
+        colorOption.value = color
+        colorOption.innerHTML = color
+        const colorContainer = document.querySelector('select')
+        colorContainer.appendChild(colorOption)
+    })
+}
+
+displayProductInfos()
+
+
+
+
+// =============================================================================
+// =============================================================================
+
+
